d23fa1d17384664493817015c195abb74b83b039
Willianr7 <rafael.net@hotmail.com.br>
2022-11-04 10:39:47 -0300

    exerciciojavascript5

---
+++ Exercicio JavaScript 5/ex3.js
@@ -0,0 +1,6 @@
+var a = Number(prompt("Digite sua idade"))
+
+ if( a < 15 ){alert(`Vc é uma criança `)}
+ if(a >= 15 && a < 18 ){alert(`Vc é um Adolecente `)}
+ if(a >= 18 ){alert(`Vc é um Adulto `)}
+ 
---
+++ Exercicio JavaScript 5/ex4.js
@@ -0,0 +1,4 @@
+var num = Number(prompt("Digite o numero"))
+var a = Number
+if(num % 2 == 0){alert(`O número é par!\n E a raiz quadrada de ${num} é ${Math.sqrt(num)} `)}
+else{let quadrado = num*num (alert(`O número é imoar!\n E a seu quadrado é ${num*num} `))}
